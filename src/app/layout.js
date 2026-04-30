e150f0d246e4aad24061783938eaaf0c44576291
samidahmed2 <samid.ahmed1993@gmail.com>
2026-04-30 23:54:57 +0300

    favicon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,18 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
 
+export const metadata = {
+  title: {
+    default: "SkillSphere",
+    template: "%s | SkillSphere",
+  },
+  description: "Learn, share, and grow your skills",
+  
+};
+
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html lang="en">
       <body>
         <Navbar />
         <Toaster />
